05aa075e9b3a4e490340905b542bb82f1566ea92
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2024-02-29 18:41:00 +0100

    Refactor VerisyncView widget to include documentation and rename to _VerisyncView

--- lib/src/verisync_view.dart
+++ lib/src/verisync_view.dart
@@ -1,14 +1,32 @@
 part of "../src/verisync_button.dart";
 
-class VerisyncView extends StatefulWidget {
+/// This class represents the Verisync view.
+///
+/// The Verisync view is a stateful widget that displays a fullscreen dialog
+/// containing a web view. It is used to authorize a user and perform certain
+/// actions based on the success or failure of the authorization process.
+///
+/// The Verisync view requires the following parameters:
+/// - [redirectUrl]: The URL to redirect to after the authorization process.
+/// - [flowId]: The ID of the flow to be used for authorization.
+/// - [clientId]: The ID of the client for which the authorization is being performed.
+/// - [onSuccess]: A callback function to be called when the authorization is successful.
+/// - [onError]: A callback function to be called when an error occurs during the authorization process.
+/// - [email]: The email address of the user performing the authorization (optional).
+/// - [metadata]: Additional metadata to be passed during the authorization process (optional).
+///
+/// The Verisync view internally uses the InAppWebView package to display the web content.
+/// It also includes an action bar with close and refresh buttons, and a progress bar
+/// to indicate the loading progress of the web view.
+
+class _VerisyncView extends StatefulWidget {
   final String redirectUrl, flowId, clientId;
   final String? email;
   final void Function(BuildContext context) onSuccess;
   final void Function(BuildContext context) onError;
   final Map<dynamic, dynamic>? metadata;
 
-  const VerisyncView({
-    super.key,
+  const _VerisyncView({
     required this.redirectUrl,
     required this.flowId,
     required this.clientId,
@@ -19,10 +37,10 @@ class VerisyncView extends StatefulWidget {
   });
 
   @override
-  State<VerisyncView> createState() => _VerisyncViewState();
+  State<_VerisyncView> createState() => _VerisyncViewState();
 }
 
-class _VerisyncViewState extends State<VerisyncView> {
+class _VerisyncViewState extends State<_VerisyncView> {
   late InAppWebViewController _webViewController;
   final GlobalKey _webViewKey = GlobalKey();
   double _progress = 0;
