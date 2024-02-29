d7885afa75c3a7513a5944dab77d0e0c6df732c4
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2024-02-29 17:30:30 +0100

    Update VerisyncButton to handle camera permissions

--- lib/src/verisync_button.dart
+++ lib/src/verisync_button.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:permission_handler/permission_handler.dart';
+
 part 'verisync_view.dart';
 
 class VerisyncButton extends StatelessWidget {
@@ -22,51 +23,83 @@ class VerisyncButton extends StatelessWidget {
     this.metadata,
   });
 
-  Future<void> requestPermissions() async {
+  Future<void> loadVerisyncView(BuildContext context) async {
     WidgetsFlutterBinding.ensureInitialized();
     var status = await Permission.camera.status;
-    if (status.isDenied) {
-      await Permission.camera.request();
-    } else if (status.isPermanentlyDenied) {
-      openAppSettings();
+    switch (status) {
+      case PermissionStatus.granted:
+        if (!context.mounted) return;
+        showVerisyncView(context);
+        break;
+      case PermissionStatus.denied:
+        await Permission.camera.request().then(
+            (value) => value.isGranted ? showVerisyncView(context) : null);
+        break;
+      case PermissionStatus.permanentlyDenied:
+        if (!context.mounted) return;
+        showAdaptiveDialog(
+          context: context,
+          builder: (builder) => AlertDialog(
+            title: const Text('Camera Permission'),
+            content: const Text('Please enable camera permission to continue'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await openAppSettings()
+                      .then((value) => Navigator.of(context).pop());
+                },
+                child: const Text('Open Settings'),
+              ),
+            ],
+          ),
+        );
+        break;
+      default:
+        break;
     }
+  }
+
+  void showVerisyncView(BuildContext context) {
+    showAdaptiveDialog(
+      context: context,
+      builder: (BuildContext dialogContext) => VerisyncView(
+        flowId: flowId,
+        redirectUrl: redirectUrl,
+        clientId: clientId,
+        email: email,
+        metadata: metadata,
+        onSuccess: onSuccess ??
+            (BuildContext dialogContext) {
+              ScaffoldMessenger.of(dialogContext).showSnackBar(
+                const SnackBar(
+                  content: Text("Verification successful"),
+                  duration: Duration(seconds: 3),
+                ),
+              );
+            },
+        onError: onError ??
+            (BuildContext dialogContext) {
+              ScaffoldMessenger.of(dialogContext).showSnackBar(
+                const SnackBar(
+                  content: Text("Verification failed"),
+                  duration: Duration(seconds: 3),
+                ),
+              );
+            },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       child: const Text('Verify your identity'),
-      onPressed: () async {
-        await requestPermissions().then(
-          (value) => showAdaptiveDialog(
-            context: context,
-            builder: (BuildContext dialogContext) => VerisyncView(
-              flowId: flowId,
-              redirectUrl: redirectUrl,
-              clientId: clientId,
-              email: email,
-              metadata: metadata,
-              onSuccess: onSuccess ??
-                  (dialogContext) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Verification successful"),
-                        duration: Duration(seconds: 3),
-                      ),
-                    );
-                  },
-              onError: onError ??
-                  (dialogContext) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Verification failed"),
-                        duration: Duration(seconds: 3),
-                      ),
-                    );
-                  },
-            ),
-          ),
-        );
+      onPressed: () {
+        loadVerisyncView(context);
       },
     );
   }
